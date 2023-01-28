d067034a2f765c24b44469fe4bf4e6eaeb9d2566
parham mehrabi <parham.mehrabi.webdev@gmail.com>
2023-01-28 15:42:36 +0330

    improve performance with multi-threading ! ! !

--- combiners.py
+++ combiners.py
@@ -1,30 +1,44 @@
+import threading
+from hamzanan.hamzan import *
 
 
 def tarkib(depths,deraza,passw_list,dinamit):
-    if depths > 1:
-        for i in range(deraza):
-            for j in range(deraza):
-                passw_list.append(f'{dinamit[i]}-{dinamit[j]}')
-                passw_list.append(f'{dinamit[i]}{dinamit[j]}')
-    if depths > 2:
-        for i in range(deraza):
-            for j in range(deraza):
-                for g in range(deraza):
-                    passw_list.append(f'{dinamit[i]}-{dinamit[j]}-{dinamit[g]}')
-                    passw_list.append(f'{dinamit[i]}{dinamit[j]}{dinamit[g]}')
-    if depths > 3:
-        for i in range(deraza):
-            for j in range(deraza):
-                for g in range(deraza):
-                    for g2 in range(deraza):
-                        passw_list.append(f'{dinamit[i]}-{dinamit[j]}-{dinamit[g]}-{dinamit[g2]}')
-                        passw_list.append(f'{dinamit[i]}{dinamit[j]}{dinamit[g]}{dinamit[g2]}')
-    if depths > 4:
-        for i in range(deraza):
-            for j in range(deraza):
-                for g in range(deraza):
-                    for g2 in range(deraza):
-                        for g3 in range(deraza):
-                            passw_list.append(f'{dinamit[i]}-{dinamit[j]}-{dinamit[g]}-{dinamit[g2]}-{dinamit[g3]}')
-                            passw_list.append(f'{dinamit[i]}{dinamit[j]}{dinamit[g]}{dinamit[g2]}{dinamit[g3]}')
+    if depths == 2:
+        passw_list = dotaii(deraza,passw_list,dinamit)
+        return passw_list
+    if depths == 3:
+        a = threading.Thread(target=dotaii, args=(deraza,passw_list,dinamit))
+        b = threading.Thread(target=setaii, args=(deraza,passw_list,dinamit))
+        a.start()
+        b.start()
+        a.join()
+        b.join()
+        return passw_list
+    if depths == 4:
+        a = threading.Thread(target=dotaii, args=(deraza,passw_list,dinamit))
+        b = threading.Thread(target=setaii, args=(deraza,passw_list,dinamit))
+        c = threading.Thread(target=chahartaii, args=(deraza,passw_list,dinamit))
+        a.start()
+        b.start()
+        c.start()
+        a.join()
+        b.join()
+        c.join()
+
+        return passw_list
+    if depths == 5:
+        a = threading.Thread(target=dotaii, args=(deraza,passw_list,dinamit))
+        b = threading.Thread(target=setaii, args=(deraza,passw_list,dinamit))
+        c = threading.Thread(target=chahartaii, args=(deraza,passw_list,dinamit))
+        d = threading.Thread(target=panjtaii, args=(deraza,passw_list,dinamit))
+        a.start()
+        b.start()
+        c.start()
+        d.start()
+        a.join()
+        b.join()
+        c.join()
+        d.join()
+        return passw_list
+
     return passw_list
---
+++ hamzanan/hamzan.py
@@ -0,0 +1,35 @@
+def dotaii(deraza,passw_list,dinamit):
+    for i in range(deraza):
+        for j in range(deraza):
+            passw_list.append(f'{dinamit[i]}-{dinamit[j]}')
+            passw_list.append(f'{dinamit[i]}{dinamit[j]}')
+    return passw_list
+
+def setaii(deraza,passw_list,dinamit):
+    for i in range(deraza):
+        for j in range(deraza):
+            for g in range(deraza):
+                passw_list.append(f'{dinamit[i]}-{dinamit[j]}-{dinamit[g]}')
+                passw_list.append(f'{dinamit[i]}{dinamit[j]}{dinamit[g]}')
+    return passw_list
+
+def chahartaii(deraza,passw_list,dinamit):
+    for i in range(deraza):
+        for j in range(deraza):
+            for g in range(deraza):
+                for g2 in range(deraza):
+                    passw_list.append(f'{dinamit[i]}-{dinamit[j]}-{dinamit[g]}-{dinamit[g2]}')
+                    passw_list.append(f'{dinamit[i]}{dinamit[j]}{dinamit[g]}{dinamit[g2]}')
+    return passw_list
+
+def panjtaii(deraza,passw_list,dinamit):
+    for i in range(deraza):
+        for j in range(deraza):
+            for g in range(deraza):
+                for g2 in range(deraza):
+                    for g3 in range(deraza):
+                        passw_list.append(f'{dinamit[i]}-{dinamit[j]}-{dinamit[g]}-{dinamit[g2]}-{dinamit[g3]}')
+                        passw_list.append(f'{dinamit[i]}{dinamit[j]}{dinamit[g]}{dinamit[g2]}{dinamit[g3]}')
+    return passw_list
+
+
